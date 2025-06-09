00f6ed7d7e204811f8c11ac8a2e7ef2da6cc6184
Shav101 <shavkat192710@gmail.com>
2025-06-09 01:28:44 -0400

    Added variable notes

---
+++ Variables/index.js
@@ -0,0 +1,47 @@
+// var is used to declare a variable
+// userName is variable name
+// PewPew is a value that is being assigned to userName
+
+// We create variables to store a value temporarily somewhere in the memory so we can reference it later!
+
+var userName = "PewPew";
+
+// ===============================================================
+/* Variable Rules */
+// ===============================================================
+
+// SHOULD HAVE A MEANINGFUL NAME
+var totalWins = 30;
+var totalLosses = 10;
+// ===============================================================
+
+// IT CANNOT CONTAIN A HYPHEN (-) OR A SPACE
+// var total-Losses = 10;
+// var total Losses = 10;
+// ===============================================================
+
+// CANNOT START WITH A NUMBER
+// var 1totalLosses = 10;
+
+// =======
+
+// BUT CAN ADD A NUMBER AT THE END
+// var totalLosses1 = 10;
+// ===============================================================
+
+// Cannot use a reserved word
+// var / let / if / true / false
+
+// ===============================================================
+
+// VARIABLES ARE CASE SENSITIVE
+// var userName = "PewPew";
+// console.log(username); this wont print anything since the userName is written wrong
+
+// ===============================================================
+
+// (var) - Prior ES2015 / ES6
+
+// NEW WAY OF DECLARING VARIABLES (let, const)
+let name = "Shav";
+const age = 25;
